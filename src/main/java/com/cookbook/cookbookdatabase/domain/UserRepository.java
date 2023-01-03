7eb16ba22dc42f772af7735c89f12d789718e366
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-03 20:03:50 +0200

    Added method for finding a user by username

--- src/main/java/com/cookbook/cookbookdatabase/domain/UserRepository.java
+++ src/main/java/com/cookbook/cookbookdatabase/domain/UserRepository.java
@@ -1,7 +1,11 @@
 package com.cookbook.cookbookdatabase.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+	
+	Optional<User> findByUsername(String username);
 
 }
