c7a91ddd4e8a60b3af42fff9793cc9b6920a1f42
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-04 20:19:20 +0200

    Method for finding ingredient by name for testing

--- src/main/java/com/cookbook/cookbookdatabase/domain/IngredientRepository.java
+++ src/main/java/com/cookbook/cookbookdatabase/domain/IngredientRepository.java
@@ -1,7 +1,12 @@
 package com.cookbook.cookbookdatabase.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface IngredientRepository extends CrudRepository<Ingredient, Long> {
-
+	
+	// For testing ingredient repository.
+	Optional<Ingredient> findByIngredientName(String ingredientName);
+	
 }
