5f20e64ad635381c2080fe006e6c6ef3c2c2317c
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-04 19:36:47 +0200

    Added method for findig an amount by quantity for testing repository

--- src/main/java/com/cookbook/cookbookdatabase/domain/AmountRepository.java
+++ src/main/java/com/cookbook/cookbookdatabase/domain/AmountRepository.java
@@ -1,7 +1,12 @@
 package com.cookbook.cookbookdatabase.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface AmountRepository extends CrudRepository<Amount, Long> {
-
+	
+	// For testing.
+	Optional<Amount> findByQuantity(String quantity);
+	
 }
