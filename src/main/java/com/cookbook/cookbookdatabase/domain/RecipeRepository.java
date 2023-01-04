bcecdf097d878dd421beae1879567c45ed32ed3b
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-04 20:26:51 +0200

    Method for testing recipe repository

--- src/main/java/com/cookbook/cookbookdatabase/domain/RecipeRepository.java
+++ src/main/java/com/cookbook/cookbookdatabase/domain/RecipeRepository.java
@@ -1,9 +1,13 @@
 package com.cookbook.cookbookdatabase.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 	
 	Iterable<Recipe> findByUser(User user);
-
+	
+	// For testing recipe repository.
+	Optional<Recipe> findByRecipeName(String recipeName);
 }
