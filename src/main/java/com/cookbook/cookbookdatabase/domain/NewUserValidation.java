f18f9b6038a443e2d7740408eec01325f9df3c14
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-09 19:27:50 +0200

    New class for validating new user's data.

---
+++ src/main/java/com/cookbook/cookbookdatabase/domain/NewUserValidation.java
@@ -0,0 +1,63 @@
+package com.cookbook.cookbookdatabase.domain;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class NewUserValidation {
+	
+	@NotBlank
+	@Size(min = 4, max = 20)
+	private String username = "";
+	
+	@NotBlank
+	@Size(min = 7, max = 32)
+	private String password = "";
+	
+	@NotBlank
+	@Size(min = 7, max = 32)
+	private String passwordCheck = "";
+	
+	@NotBlank
+	private String role = "USER";
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordCheck() {
+		return passwordCheck;
+	}
+
+	public void setPasswordCheck(String passwordCheck) {
+		this.passwordCheck = passwordCheck;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return "NewUserValidation [username=" + username + ", password=" + password + ", passwordCheck=" + passwordCheck
+				+ ", role=" + role + "]";
+	}
+	
+	
+
+}
