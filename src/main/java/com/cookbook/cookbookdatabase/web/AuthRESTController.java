e5e6362b2948317c8e131167d3fb85ef57383d48
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-09 19:30:04 +0200

    Validate data when saving a new user

--- src/main/java/com/cookbook/cookbookdatabase/web/AuthRESTController.java
+++ src/main/java/com/cookbook/cookbookdatabase/web/AuthRESTController.java
@@ -1,8 +1,5 @@
 package com.cookbook.cookbookdatabase.web;
 
-import java.util.Collections;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,14 +12,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.auth0.jwt.exceptions.JWTCreationException;
 import com.cookbook.cookbookdatabase.domain.LoginCredentials;
+import com.cookbook.cookbookdatabase.domain.NewUserValidation;
 import com.cookbook.cookbookdatabase.domain.User;
 import com.cookbook.cookbookdatabase.domain.UserRepository;
 import com.cookbook.cookbookdatabase.security.JWTUtil;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonSyntaxException;
+
+import jakarta.validation.Valid;
 
 /*
  * Controller for handling logging in and registering a new account.
@@ -41,20 +37,42 @@ public class AuthRESTController {
 	@Autowired
 	private AuthenticationManager authManager;
 	
-	// TODO: Add validations and check if the username already exist.
+	/*
+	 * Handles saving a new user.
+	 */
+	
 	@PostMapping(value = "/register")
-	public Map<String, Object> registerHandler(@RequestBody String newUserCredientals) {
-		User newUser = new User();
-		JsonObject credientals = JsonParser.parseString(newUserCredientals).getAsJsonObject();
-		String password = credientals.get("password").getAsString();
-		String encodedPassword = new BCryptPasswordEncoder().encode(password);
-		newUser.setPassword(encodedPassword);
-		newUser.setUsername(credientals.get("username").getAsString());
-		newUser.setRole("USER");
-		userRepo.save(newUser);
-		String token = jwtUtil.generateToken(newUser.getUsername());
-		return Collections.singletonMap("jwt-token", token);
+	public ResponseEntity<?> addUser(@RequestBody @Valid NewUserValidation newUser) {
+		// Check, is the password same as the passwordCheck.
+		if (newUser.getPassword().equals(newUser.getPasswordCheck())) {
+			User user = new User();
+			BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+			String password = newUser.getPassword();
+			// Encode the user's password.
+			user.setPassword(bc.encode(password));
+			// Set username
+			user.setUsername(newUser.getUsername());
+			// Set role.
+			user.setRole("USER");
+			
+			// Username must be unique.
+			if (userRepo.findByUsername(newUser.getUsername()).isEmpty()) {
+				// Save the new user.
+				userRepo.save(user);
+			} else {
+				// If the username is not unique -> return HttpStatus 409.
+				return ResponseEntity.status(HttpStatus.CONFLICT).build();
+			}
+		} else {
+			// If the passwords are different -> return HttpStatus 400.
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+		return ResponseEntity.ok().build();
 	}
+	
+	/*
+	 * Handles logging in.
+	 */
 	
 	@PostMapping(value = "/login")
 	public ResponseEntity<?> loginHandler(@RequestBody LoginCredentials body) {
@@ -67,7 +85,7 @@ public class AuthRESTController {
 			// Generates token.
 			String token = jwtUtil.generateToken(body.getUsername()); 
 			
-			// Returns token inside the headers to the front.
+			// Returns token inside the headers to the client.
 			return ResponseEntity.ok()
 					.header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
 					.header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization")
